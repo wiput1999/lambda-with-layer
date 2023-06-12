38a6214da1903d7302a6ed7a6975b7049c72805f
Wiput Pootong <wiput.pootong@gmail.com>
2023-06-12 22:37:08 +0700

    feat: initial commit

--- lib/lambda-with-layer-stack.ts
+++ lib/lambda-with-layer-stack.ts
@@ -1,16 +1,42 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import path = require("path");
 
 export class LambdaWithLayerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const layer1 = new lambda.LayerVersion(this, "example-layer-1", {
+      compatibleRuntimes: [
+        lambda.Runtime.NODEJS_14_X,
+        lambda.Runtime.NODEJS_16_X,
+      ],
+      code: lambda.Code.fromAsset("src/layer1"),
+      description: "Example Layer 1",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaWithLayerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const layer2 = new lambda.LayerVersion(this, "example-layer-2", {
+      compatibleRuntimes: [
+        lambda.Runtime.NODEJS_14_X,
+        lambda.Runtime.NODEJS_16_X,
+      ],
+      code: lambda.Code.fromAsset("src/layer2"),
+      description: "Example Layer 2",
+    });
+
+    // 👇 Lambda function
+    new NodejsFunction(this, "my-function", {
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(5),
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: "main",
+      entry: path.join(__dirname, `/../src/app/index.ts`),
+      bundling: {
+        minify: false,
+      },
+      layers: [layer1, layer2],
+    });
   }
 }
